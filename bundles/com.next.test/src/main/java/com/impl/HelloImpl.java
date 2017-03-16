1f2de690d959a67aee2ab95e0b189507f8df77b3
Mladen Kralev <mladenkralev@gmail.com>
2017-03-16 22:04:30 +0200

    Fixed compilation problem, should fix the DS problem...

--- bundles/com.next.test/src/main/java/com/impl/HelloImpl.java
+++ bundles/com.next.test/src/main/java/com/impl/HelloImpl.java
@@ -1,11 +1,11 @@
 package com.impl;
 
+import com.interfaces.IHello;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.framework.BundleActivator;
 
 @Component(service = IHello.class, name = "MySuperService", immediate = true)
-public class HelloImpl implements BundleActivator, Fubar
+public class HelloImpl implements IHello
 {
     @Activate
     public void turnOn()
