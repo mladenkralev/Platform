799535d282562d41026ad165857b3a92486e1539
Kralev <MLKR@EUR.AD.SAG>
2017-04-06 19:08:45 +0300

    Making copy tasks for all jars and all dependencies WIP

--- bundles/com.next.test/src/main/java/com/impl/HelloImpl.java
+++ bundles/com.next.test/src/main/java/com/impl/HelloImpl.java
@@ -1,13 +1,10 @@
 package com.impl;
 
 import com.interfaces.IHello;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
 
-@Component(service = IHello.class, name = "MySuperService", immediate = true)
+
 public class HelloImpl implements IHello
 {
-    @Activate
     public void turnOn()
     {
         System.out.println("FubarService activated");
