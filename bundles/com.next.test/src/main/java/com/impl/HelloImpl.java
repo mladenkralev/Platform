43cb4fb1c3f914ad175fc88610afc09aa0bac21f
Mladen Kralev <mladenkralev@gmail.com>
2017-04-08 15:22:05 +0300

    Deleted DS

--- bundles/com.next.test/src/main/java/com/impl/HelloImpl.java
+++ bundles/com.next.test/src/main/java/com/impl/HelloImpl.java
@@ -1,19 +1,20 @@
 package com.impl;
 
 import com.interfaces.IHello;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
 
-@Component(service = IHello.class, name = "MySuperService", immediate = true)
+
 public class HelloImpl implements IHello
 {
-    @Activate
     public void turnOn()
     {
         System.out.println("FubarService activated");
     }
 
-    @Override
+    public void realTurnOff()
+    {
+        System.out.println("FubarService activated");
+    }
+
     public void sayHello()
     {
         System.out.println("Hello World!");
