0fdba3a6267ee56c7f074940c3fbd2848aa92533
Mladen Kralev <mladenkralev@gmail.com>
2017-04-09 13:14:05 +0300

    Adding test module Adding subproject tests and Dummy test

--- bundles/com.bundle.activator/src/main/java/com/helloworld/HelloWorldActivator.java
+++ bundles/com.bundle.activator/src/main/java/com/helloworld/HelloWorldActivator.java
@@ -3,40 +3,44 @@ package com.helloworld;
 import org.osgi.framework.*;
 
 public class HelloWorldActivator implements BundleActivator, BundleListener {
-	
-	private BundleContext context =
-			FrameworkUtil.getBundle(HelloWorldActivator.class).getBundleContext();
 
-	@Override
-	public void bundleChanged(BundleEvent event) {
-		switch (event.getType()) {
-		case BundleEvent.INSTALLED:
-			System.out.println("Installed");
-			printBundleCount();
-			break;
-		case BundleEvent.STARTED:
-			System.out.println("started");
-			printBundleCount();
-			break;
-		default:
-			break;
-		}
-	}
+    private BundleContext context =
+            FrameworkUtil.getBundle(HelloWorldActivator.class).getBundleContext();
 
-	private void printBundleCount() {
-		Bundle[] bundlesInContext = context.getBundles();
-		for(Bundle bundle : bundlesInContext){
-			System.out.println(bundle.toString());
-		}
-	}
+    @Override
+    public void bundleChanged(BundleEvent event) {
+        switch (event.getType()) {
+            case BundleEvent.INSTALLED:
+                System.out.println("Installed");
+                printBundleCount();
+                break;
+            case BundleEvent.STARTED:
+                System.out.println("started");
+                printBundleCount();
+                break;
+            default:
+                break;
+        }
+    }
 
-	@Override
-	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Working");
-	}
+    private void printBundleCount() {
+        Bundle[] bundlesInContext = context.getBundles();
+        for (Bundle bundle : bundlesInContext) {
+            System.out.println(bundle.toString());
+        }
+    }
 
-	@Override
-	public void stop(BundleContext bundleContext) throws Exception {
-		System.out.println("stopping");
-	}
+    private int getBundleCount() {
+        return context.getBundles().length;
+    }
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("Working");
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        System.out.println("stopping");
+    }
 }
---
+++ tests/com.test.test/src/test/java/TestDummy.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Mladen on 9.4.2017 г..
+ */
+public class TestDummy {
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+
+    }
+}
